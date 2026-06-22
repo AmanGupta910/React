f0c3623f36c252d47523b18b5eea6e350ff1b7ec
Aman <amanguptawork2726@gmail.com>
2026-06-22 15:01:39 +0530

    Own react

--- ReactV/src/App.jsx
+++ ReactV/src/App.jsx
@@ -5,10 +5,11 @@ import New from './Aman.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
+  const Username="Aman&Vipil"
   return (
     <>
        <New/>
-     <h1>Hello byVite |Aman </h1>
+     <h1>Hello byVite |{Username} </h1>
 
      
     </>
--- ReactV/src/main.jsx
+++ ReactV/src/main.jsx
@@ -1,12 +1,52 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  
-  </StrictMode>,
+function MyApp(){
+  return(
+    <div>
+      <h1>Hello Aman !</h1>
+    </div>
+  )
+}
+
+
+ 
+// const ReactElement={
+//     type:'a',
+//     props: {
+//         href:'https://google.com',
+//         target:"_blank"
+//     },
+//     children:' Click me to visit google'
+// }
+
+
+
+ 
+const ReactElement=React.createElement(
+  'a',
+  {
+    href:"https://google.com",
+    target:"_blank "
+  },
+  'click me to visit '
+)
+
+
+ const anotherElement =(
+   <a href="https://google.com"  target='_blank'>Visit Google</a>
+ )
+
+
+
+createRoot(document.getElementById('root')).render( 
+    // anotherElement
+    // ReactElement
+
+    <App/>
+    /* <MyApp/> and MyApp() both works as at the end both are just functions */
+
 )
